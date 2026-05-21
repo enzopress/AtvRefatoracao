e3075283034d6fa454c3e7eae4da078e4b5933a2
enzopress <enzopedro@hotmail.com>
2026-05-21 19:39:30 -0300

    first commit

---
+++ .idea/src/OperacaoEstacionamento.java
@@ -0,0 +1,12 @@
+
+
+import java.time.Duration;
+
+public class OperacaoEstacionamento {
+
+    public long calcularMinutosPermanencia(TicketEstacionamento ticket) {
+        return Duration.between(
+                ticket.getDataHoraEntrada(),
+                ticket.getDataHoraSaidaPrevista()).toMinutes();
+    }
+}
---
+++ .idea/src/PainelVagas.java
@@ -0,0 +1,19 @@
+
+public class PainelVagas {
+
+    public String descreverStatus(Vaga vaga) {
+        if (vaga.isOcupada() && vaga.isCoberta()) {
+            return "ocupada e coberta";
+        }
+
+        if (vaga.isOcupada()) {
+            return "ocupada";
+        }
+
+        if (vaga.isCoberta()) {
+            return "livre e coberta";
+        }
+
+        return "livre";
+    }
+}
